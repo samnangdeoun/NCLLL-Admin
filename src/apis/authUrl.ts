a0f712e5f76b2a2ac767ef6cb035545fe86a539c
Deounsamnang <Deounsamnang7@gmail.com>
2025-03-26 13:44:07 +0700

    Set Up Image Upload.

--- src/apis/authUrl.ts
+++ src/apis/authUrl.ts
@@ -3,6 +3,7 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 const authAPI = () => {
     return {
         user_login               :       BASE_URL + "/auth/login",
+        file_upload              :       BASE_URL + "/upload",
     }
 }
 
